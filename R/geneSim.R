78ceb9401802b447510447bc9c34520008de1388
guangchuang yu <guangchuangyu@gamil.com>
2017-01-03 11:26:51 +0800

    update man

--- R/geneSim.R
+++ R/geneSim.R
@@ -1,12 +1,12 @@
 ##' semantic similarity between two gene vector
 ##'
-##' 
+##'
 ##' @title geneSim
-##' @param geneID1 gene ID vector 
-##' @param geneID2 gene ID vector 
+##' @param geneID1 gene ID vector
+##' @param geneID2 gene ID vector
 ##' @param measure one of "Wang", "Resnik", "Rel", "Jiang" and "Lin"
-##' @param combine One of "max", "average", "rcmax", "BMA" methods, for combining semantic similarity scores of multiple DO terms associated with gene/protein.
-##' @param semData gene annotation data for semantic measurement 
+##' @param combine One of "max", "avg", "rcmax", "BMA" methods, for combining semantic similarity scores of multiple DO terms associated with gene/protein.
+##' @param semData gene annotation data for semantic measurement
 ##' @importFrom GOSemSim combineScores
 ##' @return score matrix
 ##' @export
@@ -14,7 +14,7 @@
 ##' ## hsamd <- meshdata("MeSH.Hsa.eg.db", category='A', computeIC=T, database="gendoo")
 ##' data(hsamd)
 ##' geneSim("241", "251", semData=hsamd, measure="Wang", combine="BMA")
-##' @author Guangchuang Yu 
+##' @author Guangchuang Yu
 geneSim <- function(geneID1,
                     geneID2=NULL,
                     measure="Wang",
@@ -22,7 +22,7 @@ geneSim <- function(geneID1,
                     semData) {
 
     if (!exists(".meshesEnv")) .initial()
-    
+
     meshid1 <- lapply(geneID1, gene2MeSH, semData=semData)
     if (is.null(geneID2)) {
         geneID2 <- geneID1
@@ -36,7 +36,7 @@ geneSim <- function(geneID1,
     scores <- matrix(NA, nrow=m, ncol=n)
     rownames(scores) <- geneID1
     colnames(scores) <- geneID2
-    
+
     for (i in 1:m) {
         if (length(geneID1) == length(geneID2) && all(geneID1 == geneID2)) {
            nn <- i
@@ -45,7 +45,7 @@ geneSim <- function(geneID1,
             flag <- FALSE
             nn <- n
         }
-        
+
         for (j in 1:nn) {
             if(any(!is.na(meshid1[[i]])) &&  any(!is.na(meshid2[[j]]))) {
                 s <- meshSim(meshid1[[i]],
