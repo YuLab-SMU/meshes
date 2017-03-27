14967969309a8f0ca3676848b89370272ca33b72
Guangchuang Yu <guangchuangyu@gmail.com>
2017-03-27 02:00:24 +0000

    update docs
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/meshes@127725 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DOSE.R
+++ R/DOSE.R
@@ -2,6 +2,6 @@ build_Anno <- DOSE:::build_Anno
 enricher_internal <- DOSE:::enricher_internal
 GSEA_internal <- DOSE:::GSEA_internal
 get_organism <- DOSE:::get_organism
-get_fun_from_pkg <- DOSE:::get_fun_from_pkg
+
 
 
