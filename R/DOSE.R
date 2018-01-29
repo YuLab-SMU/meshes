c266a8c3667fe3a68e14c6e77bb102386525f1ed
guangchuang yu <guangchuangyu@gamil.com>
2018-01-29 21:46:14 +0800

    enrichplot support

--- R/DOSE.R
+++ R/DOSE.R
@@ -1,7 +1,17 @@
+##' @importMethodsFrom DOSE show
+##' @importMethodsFrom DOSE summary
+
+##' @importFrom DOSE geneID
+##' @export
+DOSE::geneID
+
+##' @importFrom DOSE geneInCategory
+##' @export
+DOSE::geneInCategory
+
+
 build_Anno <- DOSE:::build_Anno
 enricher_internal <- DOSE:::enricher_internal
 GSEA_internal <- DOSE:::GSEA_internal
 get_organism <- DOSE:::get_organism
-
-
 
