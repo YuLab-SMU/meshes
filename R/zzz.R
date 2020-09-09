ee2cc7430ef1194a4cce21343a7d22f13f895629
Guangchuang Yu <guangchuangyu@gmail.com>
2020-09-09 11:41:54 +0800

    update docs

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,8 @@
     pkgVersion <- packageDescription(pkgname, fields="Version")
     msg <- paste0(pkgname, " v", pkgVersion, "  ", "\n\n")
     citation <- paste0("If you use ", pkgname, " in published research, please cite the most appropriate paper(s):\n\n",
-                       "Guangchuang Yu.",
-                       "Using meshes for MeSH term enrichment and semantic analyses.",
+                       "Guangchuang Yu. ",
+                       "Using meshes for MeSH term enrichment and semantic analyses. ",
                        "Bioinformatics 2018, 34(21):3766-3767, doi:10.1093/bioinformatics/bty410")
     
     packageStartupMessage(paste0(msg, citation))
